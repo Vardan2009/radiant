49f10f613407d8ef5ead6c1a09647a19781186a5
Vardan Petrosyan <petrosyanvardan2009@gmail.com>
2024-07-09 22:46:59 +0400

    Add project files.

---
+++ radiant/Kernel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace radiant
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Radiant booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("> ");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
